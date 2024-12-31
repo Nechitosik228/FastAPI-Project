731ea5b94e1fd9f5751586539519e298f2e3eec1
Yar00myr <ivanucaromir@gmail.com>
2024-12-31 20:45:36 +0000

    add pydantic model and start write crud for comments

--- backend/routes/comment.py
+++ backend/routes/comment.py
@@ -1,3 +1,14 @@
-from fastapi import APIRouter, HTTPException, Depends
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
 
 
+from ..db import Comment
+from ..schemas import UserModel
+
+
+comment_router = APIRouter(prefix="/comments", tags=["Comments"])
+
+@comment_router.post("/create", status_code=status.HTTP_201_CREATED)
+def create_comment(data: Comment, session:Annotated[Session]):
+    None
