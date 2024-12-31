adc2b0686f932800ddf2a14e416c53e031f4068a
Yar00myr <ivanucaromir@gmail.com>
2024-12-31 18:21:28 +0000

    ...

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,21 +1,21 @@
-from .models import (
-    Config,
-    User,
-    Comment,
-    Post
-)
+# from .models import (
+#     Config,
+#     User,
+#     Comment,
+#     Post
+# )
 
 
-def up():
-    Config.BASE.metadata.create_all(Config.ENGINE)
+# def up():
+#     Config.BASE.metadata.create_all(Config.ENGINE)
 
 
-def down():
-    Config.BASE.metadata.drop_all(Config.ENGINE)
+# def down():
+#     Config.BASE.metadata.drop_all(Config.ENGINE)
 
 
-def migrate():
-    down()
-    up()
+# def migrate():
+#     down()
+#     up()
 
 
